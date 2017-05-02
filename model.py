2cd4590a864e175349506e6f9ba7853f9af6b41f
Sagar Udasi <Sagar Udasi>
2017-05-02 20:12:35 +0530

    Organize code

--- model.py
+++ model.py
@@ -1,17 +1,5 @@
 import os
 import csv
-
-samples = []
-basepath = "./dataset/"
-with open(basepath + 'driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
-
-from sklearn.model_selection import train_test_split
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-batch_size = 64
-
 import cv2
 import numpy as np
 import sklearn
@@ -22,24 +10,37 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Cropping2D
 from keras.layers import Dense, Dropout, Activation, Flatten, Lambda
 from keras import backend as k
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
-# batch_samples = samples[0:2]
+samples = []
+basepath = "./dataset/"
 
-# images = []
-# angles = []
-# for batch_sample in batch_samples:
-#     name = basepath+'IMG/'+batch_sample[0].split('\\')[-1]
-#     print(name)
-#     center_image = cv2.imread(name)
-#     center_angle = float(batch_sample[3])
-#     images.append(center_image)
-#     angles.append(center_angle)
+# Load the data from CSV files into memory
+with open(basepath + 'driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
 
-# # trim image to only see section with road
-# X_train = np.array(images)
-# y_train = np.array(angles)
+# Split the data into training set and validation set in 4:1 ratio
+train_samples, validation_samples = train_test_split(samples, test_size=0.3)
 
-# print(X_train)
+# Decide hyperparameters 
+batch_size = 64
+epochs = 2
+
+# original image dimensions
+orow, ocol = 160, 320
+input_channels = 3
+
+# Cropping2D parameters
+ctop = 60
+cbottom = 20
+cleft = 0
+cright = 0
+
+# Cropped image dimensions
+row, col = (orow - (ctop + cbottom)), (ocol - (cleft + cright))
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -70,30 +71,43 @@ def generator(samples, batch_size=32):
 train_generator = generator(train_samples, batch_size=batch_size)
 validation_generator = generator(validation_samples, batch_size=batch_size)
 
-ch, row, col = 3, 80, 320  # Trimmed image format
-
-
 # Check the backend and image data format
-# If the channel info is to be stored first or last
+# set the image data format to channels_first
 k.set_image_data_format('channels_first')
 
 model = Sequential()
-# Preprocess incoming data, centered around zero with small standard deviation 
-model.add(Cropping2D(cropping=((60,20), (0,0)), input_shape=(3, 160,320)))
-model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(ch, row, col), output_shape=(ch, row, col)))
-model.add(Conv2D(16,(3,3), activation='relu', input_shape=(3, row,col)))
+
+# Preprocessing 
+model.add(Cropping2D(cropping=((ctop,cbottom), (cleft,cright)), input_shape=(input_channels, orow, ocol)))
+model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(input_channels, row, col), output_shape=(input_channels, row, col)))
+
+# Convolution layers 
+model.add(Conv2D(16,(3,3), activation='relu', input_shape=(input_channels, row,col)))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
 model.add(Conv2D(32,(3,3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
+
+# Flatten 
 model.add(Flatten())
+
+# Fully connected layers
 model.add(Dense(256, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, steps_per_epoch=(len(train_samples)/batch_size), validation_data=validation_generator, validation_steps=len(validation_samples), epochs=2)
+history_object = model.fit_generator(train_generator, steps_per_epoch=(len(train_samples)/batch_size), validation_data=validation_generator, validation_steps=(len(validation_samples)/batch_size), epochs=epochs)
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
 
 # X_train, y_train = (next(train_generator))
 # print(X_train.shape)
